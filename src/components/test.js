3e7a3021dbe7bd4d1a04fb5ae76ccd3a7e52a8fa
Mike Landers <landers.mike@gmail.com>
2019-05-21 10:04:30 -0700

    setup auth

---
+++ src/components/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Authenticate from './auth';
+
+const Test = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+};
+
+export default Authenticate(Test);
