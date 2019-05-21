fa972f51d7a051455a6452916f02b75db4fc3c58
Mike Landers <landers.mike@gmail.com>
2019-05-21 10:55:28 -0700

    added firebase key info to gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Test from './components/test';
+import Home from './components/marketing/home';
+import Index from './components/app/index';
+
+const user = localStorage.getItem('firebaseui::rememberedAccounts');
 
 function App() {
-    return (
-        <div>
-            <Test />
-        </div>
-    );
+    return <div>{user ? <Index /> : <Home />}</div>;
 }
 
 export default App;
