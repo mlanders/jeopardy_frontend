130ca5646ba9f7fec854c79897b833b05ae3aead
Mike Landers <landers.mike@gmail.com>
2019-05-21 11:33:54 -0700

    clean commit

---
+++ src/components/authentication/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyCyGl5aEUmjZWeEyhE6kMy6oNyj2gvqH5I',
+    authDomain: 'jeopardy-f1d97.firebaseapp.com',
+    databaseURL: 'https://jeopardy-f1d97.firebaseio.com',
+    projectId: 'jeopardy-f1d97',
+    storageBucket: 'jeopardy-f1d97.appspot.com',
+    messagingSenderId: '822812740268',
+    appId: '1:822812740268:web:6d9633ac6039b7ea'
+};
+export default firebaseConfig;
